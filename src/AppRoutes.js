1a1626cfdb812fd62364ab9d5b250d490000d2f1
Michael Armstrong <mjay624@gmail.com>
2021-08-20 09:22:20 +0100

    switch to hashrouter

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Team from './pages/Team'
@@ -12,7 +12,7 @@ import JobListing from './components/JobListing'
 
 const AppRoutes = () => {
     return (
-        <Router basename='/plum-company-site'>
+        <Router>
             <Layout>
                 <Switch>
                     <UnauthorizedAppRoutes />
